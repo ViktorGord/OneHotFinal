17abbabe8f30162dd2bbcf7ec5ce9779b173aff5
Viktor Gordikov <flaiho@bk.ru>
2023-12-23 20:48:58 +0300

    готовая версия

---
+++ OneHot.py
@@ -0,0 +1,14 @@
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.head()
+
+data['temp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace = True)
+data = data.unstack(level = -1, fill_value = 0).astype (int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
